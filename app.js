effde2c01987368bd7882061e9f98cad5a651e3c
ShanzaGul <Gulshanza77@gmail.com>
2020-10-18 23:32:04 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("yes")
+function cool(n){
+
+  return (n%3==0 && n%5==0) ? console.log("fizzbuzz")
+         : (n%3==0) ? console.log("fizz")
+         : (n%5==0) ? console.log("buzz")
+         : n;
+}
